a0dd245973e766b82aef5923f67a976939a2981f
moon0900 <moon09@duksung.ac.kr>
2025-04-05 12:32:37 +0900

    [feat] handlers: FastAPI 전역 예외 처리 핸들러 추가

--- Prompting/exceptions/__init__.py
+++ Prompting/exceptions/__init__.py
@@ -1,1 +1,8 @@
 from .errors import GeminiError, MongoAccessError, DataLoaderError
+from .handlers import (
+    gemini_exception_handler,
+    mongo_exception_handler,
+    dataloader_exception_handler,
+    request_validation_exception_handler,
+    general_exception_handler
+)
