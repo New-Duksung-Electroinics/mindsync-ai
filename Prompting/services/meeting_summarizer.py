0c1f830f8f7d8bc26dd1887f04c0dddd377b656c
moon0900 <moon09@duksung.ac.kr>
2025-04-07 20:41:46 +0900

    refactor: MeetingSummarizer 내부 로직 개선 및 Summary Use Case 일부 제거
    - 요약 생성 로직 개선:
      - 요약 생성 분할 처리 및 프롬프트 빌드 방식 개선
      - Gemini API 호출 시 필요한 MeetingHistoryBuilder를 외부에서 전달받지 않고 내부에서 직접 초기화.
    - 에러 처리 강화:
      - API 호출 및 응답 파싱 중 발생하는 예외를 `catch_and_raise` 데코레이터를 사용하여 서비스 내부에서 직접 커스텀 예외로 감싸도록 변경.
    - 불필요한 Summary Use Case 제거:
      - Gemini API 호출 및 요약 결과 DB 저장을 별도의 Summary Use Case로 구현하지 않고 엔드포인트에서 직접 처리.
    - 함수명, 타입 힌트 및 Docstring 개선 및 중복된 count_tokens 메소드 정의 제거.

--- Prompting/services/meeting_summarizer.py
+++ Prompting/services/meeting_summarizer.py
@@ -1,31 +1,33 @@
-"""
-Gemini API를 사용하여 회의록을 요약하는 클래스
+import itertools
 
-회의 채팅 내역을 기반으로 각 안건별 주요 내용을 정리하고 결론을 도출.
-안건별 주요 내용 정리는 각 발언자의 의견과 태도를 정리하는 형식으로 생성.
-생성된 요약은 JSON 형식으로 반환.
-"""
 from .gemini_client import GeminiClient
-from google.genai import types
+from google.genai.types import GenerateContentConfig
 from .templates import SUMMARY_PROMPT_EN
-
-
-INPUT_TOKEN_LIMIT = 1048576     # Limit of Gemini 2.0 flash 입력 토큰 제한 수
+from Prompting.usecases.meeting_context import MeetingContext
+from Prompting.exceptions.decorators import catch_and_raise
+from Prompting.exceptions.errors import GeminiCallError, GeminiParseError, MongoAccessError, PromptBuildError
+from .context_builders import MeetingHistoryBuilder
+from typing import cast
 
 
 class MeetingSummarizer:
-    def __init__(self, temperature=1, top_p=0.95, top_k=40, max_output_tokens=8192):
+    def __init__(self, temperature: float = 1, top_p: float = 0.95, top_k: int = 40,
+                 max_output_tokens: int = GeminiClient.OUTPUT_TOKEN_LIMIT):
         """
-        MeetingSummarizer 클래스 생성자
+        Gemini API를 사용하여 회의록을 요약하는 클래스
 
-        :param temperature: 모델의 온도 설정 (기본값: 1, 설정 가능 범위: 0~2)
-        :param top_p: (단어의) 확률 기반 샘플링을 위한 top_p 값 (기본값: 0.95)
-        :param top_k: (단어의) 확률 기반 샘플링을 위한 top_k 값 (기본값: 40)
-        :param max_output_tokens: 최대 출력 토큰 수 (기본값: 8192, 최댓값)
+        Note:
+            - 회의 채팅 내역을 기반으로 각 안건별 주요 내용을 정리하고 결론을 도출
+            - 안건별 주요 내용 정리는 각 발언자의 의견과 태도를 정리하는 형식으로 생성
+
+        Args:
+            temperature: 모델의 온도 설정 (기본값: 1, 설정 가능 범위: 0~2)
+            top_p: (단어의) 확률 기반 샘플링을 위한 top_p 값 (기본값: 0.95)
+            top_k: (단어의) 확률 기반 샘플링을 위한 top_k 값 (기본값: 40)
+            max_output_tokens: 최대 출력 토큰 수 (기본값: 8192 -> *Gemini 출력 토큰 최댓값)
         """
         self.client = GeminiClient()  # Gemini API 클라이언트 초기화
         self.template = SUMMARY_PROMPT_EN  # 회의록 생성을 위한 프롬프트 템플릿
-        self.chat_history_token_alloc = INPUT_TOKEN_LIMIT - self.count_tokens(self.generate_prompt(''))  # 채팅 기록에 할당할 토큰 수 계산
 
         # 모델 config 값 설정
         self.temperature = temperature
@@ -54,52 +56,22 @@ class MeetingSummarizer:
             'type': 'ARRAY',  # 위 요소를 갖는 (안건별) 요약 object가 담긴 배열을 반환하도록 정의
         }
 
-    def generate_prompt(self, chat_history: str):
+    @catch_and_raise("Gemini 요약 생성", GeminiCallError)
+    async def generate_summary(self, meeting_context: MeetingContext) -> list[dict]:
         """
-        프롬프트 템플릿에 채팅 내역 텍스트를 첨부하여 프름프트를 생성.
-
-        :param chat_history: 채팅 내역 텍스트, str
-        :return: 생성된 프롬프트, str
-        """
-        return self.template.format(chat_history=chat_history)
-
-    def generate_prompts(self, chat_history: list):
-        """
-        여러 개의 채팅 내역 텍스트를 첨부하여 프름프트를 생성.
-        토큰 수 제한으로 인한 분할 처리에 활용 가능.
-
-        :param chat_history: 채팅 내역 텍스트 목록, str list
-        :return: 생성된 프롬프트 목록, str list
-        """
-        prompts = []
-        for c in chat_history:
-            prompts.append(self.template.format(chat_history=c))
-        return prompts
-
-    def count_tokens(self, text):
-        """
-        텍스트의 토큰 수 계산
-
-        :param text: 토큰 수를 계산할 텍스트, str
-        :return: 텍스트의 토큰 수, int
-        """
-        token_cnt = self.client.count_tokens(text)
-        return token_cnt
-
-    async def generate_summary(self, history_builder):
-        """
-        Gemini API를 사용하여 회의 요약을 생성.
+        Gemini API로 회의 요약을 생성.
         채팅 내역이 길어질 시 분할 처리가 필요할 수 있으므로 다수의 요청을 처리하여 응답을 병합할 수 있도록 구현.
 
-        :param history_builder: 회의 채팅 내역 데이터 로더 객체, MeetingDataLodaer
-        :return: 생성된 회의 요약 데이터, JSON 형식 (안건별 요약 dict가 담긴 list)
+        Args:
+            meeting_context: 회의 맥락이 담긴 data class 객체 (주제, 안건, 채팅 내역, 주최자와 참여자)
+
+        Returns:
+            Gemini 응답 파싱 결과 (회의 요약 정보가 담긴 dict 리스트)
         """
-        # 채팅 내역 텍스트를 목록으로 준비(토큰 수 제한 고려해 필요 시 분할 처리)
-        chat_history = history_builder.build_prompt_chunks(count_tokens_callback=self.count_tokens,
-                                                           token_alloc=self.chat_history_token_alloc)
-        result = []
-        prompts = self.generate_prompts(chat_history)   # 요청 프롬프트 목록을 생성
-        config = types.GenerateContentConfig(   # Gemini API 상세 설정
+        history_builder = MeetingHistoryBuilder(meeting_context)
+        prompt_list = self._build_prompt_list(history_builder=history_builder)
+
+        config = GenerateContentConfig(  # Gemini API 상세 설정
             temperature=self.temperature,
             top_p=self.top_p,
             top_k=self.top_k,
@@ -108,12 +80,29 @@ class MeetingSummarizer:
             response_schema=self.response_schema
         )
 
-        response = await self.client.process_prompts(prompts, config)   # 비동기 처리로 요청 프롬프트 목록을 한 꺼번에 처리
-        for r in response:
-            result += r.parsed  # 응답 결과 병합
-        return result
+        # 요청 프롬프트가 입력 토큰 수 제한을 넘어 분할 처리되었을 때와 아닐 때의 로직 분기
+        if len(prompt_list) > 1:
+            responses = await self.client.process_prompts(prompt_list, config)  # 비동기 처리로 다수의 요청을 한 번에 처리
+            parsed_responses = [r.parsed for r in responses]
+            summary_data = list(itertools.chain.from_iterable(parsed_responses))  # 파싱한 다수의 응답 결과 병합
+        else:
+            response = self.client.generate_content(prompt_list[0], config)
+            summary_data = response.parsed  # 단일 응답
 
-    def parse_response_to_summary_data(self, response):
+        return cast(list[dict], summary_data)
+
+    @catch_and_raise("Gemini 요약 응답 파싱", GeminiParseError)
+    def parse_response_to_summary_data(self, response: list[dict]) -> list[dict]:
+        """
+        Gemini의 응답을 MongoDB 저장 형식에 맞게 변환
+
+        Args:
+            response: Gemini 응답으로부터 파싱된 회의 요약 list[dict]
+
+        Returns:
+            안건별 요약이 담긴 list[dict]
+            안건별 요약 dict는 안건 번호(agendaId), 안건명(topic), 요약 텍스트(content)로 구성
+        """
         summary_list = []
         for data in response:
             step = data.get("step", '')
@@ -136,3 +125,31 @@ class MeetingSummarizer:
             summary_list.append(agenda_summary)
 
         return summary_list
+
+    def _build_prompt_list(self, history_builder: MeetingHistoryBuilder) -> list[str]:
+        """
+        프롬프트 템플릿에 필요한 요소를 삽입하여 최종 프롬프트를 생성
+
+        Note:
+            - 토큰 수 제한으로 인해 회의 전체 Context를 하나의 요청 프롬프트에 담지 못할 시 분할 처리 적용
+            - 분할 처리 적용 가능성이 있으므로 프롬프트를 항상 리스트에 담아 반환
+
+        Args:
+            history_builder: 회의 맥락을 관리하고 프롬프트에 필요한 chunk를 생성하는 MeetingHistoryBuilder
+
+        Returns:
+            회의 요약 생성 요청 프롬프트가 담긴 리스트
+        """
+        # 회의 전체 Context(채팅 기록)에 할당할 토큰 수 계산
+        prompt_base = self.template.format(chat_history='')
+        history_token_alloc = GeminiClient.INPUT_TOKEN_LIMIT - self.client.count_tokens(prompt_base)
+
+        # 회의 Context 문자열 빌드, 토큰 수 제한에 맞춰 분할 처리 적용
+        chunks = history_builder.build_prompt_chunks(
+            count_tokens_callback=self.client.count_tokens,
+            token_alloc=history_token_alloc
+        )
+
+        prompt_list = [self.template.format(chat_history=chunk) for chunk in chunks]
+        return prompt_list
+
